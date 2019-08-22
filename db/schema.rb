e53332b885d919782ed4aa958b6f92b6c3e6d33f
Winnie Khuu <khuu.winnie@gmail.com>
2019-08-22 01:09:51 +0000

    Added building to room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_155058) do
+ActiveRecord::Schema.define(version: 2019_08_22_004140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "rooms", force: :cascade do |t|
-    t.string "room_number"
+    t.string "building"
+    t.integer "room_num"
+    t.string "department"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

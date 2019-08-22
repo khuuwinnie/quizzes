b274b338b46c2ae445cc7a722b16077a20c33786
Winnie Khuu <khuu.winnie@gmail.com>
2019-08-22 01:58:03 +0000

    Fixed duplicate showing of data on index page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.all
+    @rooms = Room.order('created_at desc').paginate(:per_page => 5, :page => params[:page])
+
   end
 end
